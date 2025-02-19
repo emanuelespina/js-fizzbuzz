94083ea1db0e9ae1ca2b857ad15dadf5948fccd1
emanuelespina <spina.emanuele98@gmail.com>
2025-02-19 10:27:59 +0100

    fead: comment

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+/*
+
+1. creare un ciclo che mi fornisca i num. da 1 a 100 incrementandoli a +1 
+
+2. creare due variabili che per i num. divisibili per 3 e 5
+
+3. tramite la codizione del resto capire quale tra i num. in succezzione sono divisibili sia per 3 che per 5 (questa è la condizione + specifica che va messa all'inizio)
+
+4. tramite la codizione del resto capire quale tra i num. in succezzione sono divisibili sia per 3
+
+5. tramite la codizione del resto capire quale tra i num. in succezzione sono divisibili sia per 5
+
+6. in base al risultato che abbiamo trovato stampare in console
+
+*/
+
